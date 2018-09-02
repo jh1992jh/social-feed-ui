b76894549904bb52f4f6b1cc438dc4ad27ef235d
jh1992jh <janihanninen@hotmail.com>
2018-09-02 20:42:24 +0300

    Changed package.json

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./api/routes/users');
 const posts = require('./api/routes/posts');
@@ -33,6 +34,14 @@ app.use('/api/users', users);
 app.use('/api/posts', posts);
 app.use('/api/profiles', profiles);
 app.use('/api/stories', stories);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+  }
 
 const port = process.env.PORT || 5000;
 
